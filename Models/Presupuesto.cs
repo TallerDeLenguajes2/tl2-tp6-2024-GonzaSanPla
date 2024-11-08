9091ed24c7f44a9ee17177cb848d74806d088b97
Santillán Plasencia Gonzalo <santillangon@gmail.com>
2024-11-08 17:56:05 -0300

    Corrijo unos nombres y agrego la base de datos

--- Models/Presupuesto.cs
+++ Models/Presupuesto.cs
@@ -7,15 +7,15 @@ namespace espacioPresupuestos;
 public class Presupuesto
 {
     private int idPresupuesto;
-    private string nombreDestinatario;
-    private string fechaCreacion;
+    private string? nombreDestinatario;
+    private string? fechaCreacion;
     private List<PresupuestoDetalle> detalle;
 
     const int IVA = 21;
     public int IdPresupuesto { get => idPresupuesto; set => idPresupuesto = value; }
-    public string NombreDestinatario { get => nombreDestinatario; set => nombreDestinatario = value; }
+    public string? NombreDestinatario { get => nombreDestinatario; set => nombreDestinatario = value; }
     public List<PresupuestoDetalle> Detalle { get => detalle; }
-    public string FechaCreacion { get => fechaCreacion; set => fechaCreacion = value; }
+    public string? FechaCreacion { get => fechaCreacion; set => fechaCreacion = value; }
 
     public Presupuesto()
     {
