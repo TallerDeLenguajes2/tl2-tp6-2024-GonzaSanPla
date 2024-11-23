588ec51ee40bfd7b1a54c5a87fab260b3f3741b4
Santillán Plasencia Gonzalo <santillangon@gmail.com>
2024-11-22 21:17:08 -0300

    Agrego las validaciones y temino TP7

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace espacioCliente;
 public class Cliente
 {
@@ -8,7 +10,10 @@ public class Cliente
     private string? telefono;
 
     public int ClienteId { get => clienteId; set => clienteId = value; }
+    [Required]
     public string? Nombre { get => nombre; set => nombre = value; }
+    [EmailAddress]
     public string? Email { get => email; set => email = value; }
+    [Phone]
     public string? Telefono { get => telefono; set => telefono = value; }
 }
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,13 +1,22 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace espacioProducto;
 
 public class Producto
 {
     private int idProducto;
+
     private string? descripcion;
+   
     private int precio;
 
     public int IdProducto { get => idProducto; set => idProducto = value; }
+    
+    [Required] //Porque si entra en blanco se genera mal la consulta SQL
+    [StringLength(250)]
     public string ?Descripcion { get => descripcion; set => descripcion = value; }
+ [Required]
+    [Range(0,999999)]
     public int Precio { get => precio; set => precio = value; }
 }
